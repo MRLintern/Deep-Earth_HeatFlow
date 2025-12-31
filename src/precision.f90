a80111525882636975366f027edf3185583dd0a6
Matt Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2025-12-31 12:00:40 +0000

    initial commit precision.f90

---
+++ src/precision.f90
@@ -0,0 +1,8 @@
+! Module defining the floating-point precision used throughout the simulation
+
+module precision
+
+  implicit none
+  integer, parameter :: dp = kind(1.0d0)  ! double precision kind
+  
+end module precision
